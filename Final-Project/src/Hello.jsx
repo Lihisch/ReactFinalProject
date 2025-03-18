e60fccce5be76c9c6cebab23e21c4fe449af473a
Lihisch <lihischneider3@gmail.com>
2025-03-18 20:24:18 +0200

    material ui

---
+++ Final-Project/src/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <h1>Hello from App Component!</h1>
+      </header>
+      <div>HEY hey</div>
+    </div>
+  )
+}
