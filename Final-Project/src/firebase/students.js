c79b505c4fd55328e0bf4702111ff7d57c137819
Lihisch <lihischneider3@gmail.com>
2025-05-20 22:11:07 +0300

    student.js update and set

--- Final-Project/src/firebase/students.js
+++ Final-Project/src/firebase/students.js
@@ -1,13 +1,29 @@
-import { addDoc, collection, getDocs } from "firebase/firestore"
+import { addDoc, collection, getDocs, doc, getDoc, setDoc } from "firebase/firestore"
 import { firestore } from "./firebase-settings"
 
 export async function addStudent(student) {
-return addDoc(collection(firestore, "students"), student);
+  return addDoc(collection(firestore, "students"), student);
 }
 
 export async function listStudents() {
-    const snapshot = await getDocs(collection(firestore, "students"));
-   
-    return snapshot.docs.map((doc) =>
-        ({ ...doc.data(), id: doc.id }));
-        }
+  const snapshot = await getDocs(collection(firestore, "students"));
+
+  return snapshot.docs.map((doc) =>
+    ({ ...doc.data(), id: doc.id }));
+}
+
+export async function getStudent(studentId) {
+  const studentDocRef = doc(firestore, "students", studentId);
+  const studentDocSnap = await getDoc(studentDocRef);
+
+  if (studentDocSnap.exists()) {
+    return { ...studentDocSnap.data(), id: studentDocSnap.id };
+  } else {
+    return null;
+  }
+}
+export async function updateStudent(student) {
+  const { id, ...studentData } = student;
+  const studentRef = doc(firestore, "students", id);
+  return setDoc(studentRef, studentData);
+}
